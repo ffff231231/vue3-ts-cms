eb6df2e0ce69255b34ab1075869fd8a2217c3657
FFFF231 <jiadenghui231@gmail.com>
2023-05-06 01:11:17 +0800

    集成Pinia

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="main">
-    <h2>main</h2>
+    <h2>main: {{ counter }}-{{ doubleCounter }}</h2>
+    <button @click="changeCounter">修改counter</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useCounterStore from '@/store/counter'
+import { storeToRefs } from 'pinia'
+
+const counterStore = useCounterStore()
+const { counter, doubleCounter } = storeToRefs(counterStore)
+const { changeCounterAction } = counterStore
+
+function changeCounter() {
+  changeCounterAction(1000)
+}
+</script>
 
 <style lang="less" scoped>
 .main {
