cd94c4cf6652ec7fb16f0375dc4e616fc048460d
FFFF231 <jiadenghui231@gmail.com>
2023-05-06 01:40:22 +0800

    集成axios

---
+++ src/service/config/index.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://123.207.32.32:5000'
+export const TIME_OUT = 10000
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -4,7 +4,17 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import hyRequest from '@/service'
+
+hyRequest
+  .get({
+    url: '/home/multidata'
+  })
+  .then((res) => {
+    console.log(res)
+  })
+</script>
 
 <style lang="less" scoped>
 .login {
