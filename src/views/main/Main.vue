76af3ca0e0ea98f125c150d19016b270d677c418
FFFF231 <jiadenghui231@gmail.com>
2023-05-08 23:03:03 +0800

    实现main页面的退出登录功能

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="main">main</div>
+  <div class="main">
+    <h1>main</h1>
+    <button @click="quit">退出登录</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function quit() {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
+</script>
 
 <style lang="less" scoped>
 .main {
