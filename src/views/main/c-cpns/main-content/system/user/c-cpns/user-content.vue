6cfe55924f212410b07d80c5f7ee472bc386e144
FFFF231 <jiadenghui231@gmail.com>
2023-05-20 10:53:16 +0800

    在user-contnet组件中使用作用域插槽，将状态栏的数字展示改成按钮展示

--- src/views/main/c-cpns/main-content/system/user/c-cpns/user-content.vue
+++ src/views/main/c-cpns/main-content/system/user/c-cpns/user-content.vue
@@ -5,9 +5,50 @@
       <el-button type="primary">新建用户</el-button>
     </div>
     <div class="table">
-      <ul>
-        <li v-for="item in usersList" :key="item.id">{{ item.name }}</li>
-      </ul>
+      <el-table :data="usersList" border style="width: 100%">
+        <el-table-column align="center" type="selection" width="50" />
+        <el-table-column align="center" type="index" label="序号" width="60" />
+        <el-table-column
+          align="center"
+          prop="name"
+          label="用户名"
+          width="150"
+        />
+        <el-table-column
+          align="center"
+          prop="realname"
+          label="真实姓名"
+          width="150"
+        />
+        <el-table-column
+          align="center"
+          prop="cellphone"
+          label="电话号码"
+          width="150"
+        />
+        <el-table-column align="center" prop="enable" label="状态" width="100">
+          <template #default="slotProps">
+            <el-button
+              :type="slotProps.row.enable ? 'success' : 'danger'"
+              size="small"
+              plain
+            >
+              {{ slotProps.row.enable ? '启用' : '禁用' }}
+            </el-button>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" prop="createAt" label="创建时间" />
+        <el-table-column align="center" prop="updateAt" label="更新时间" />
+        <el-table-column align="center" label="操作" width="150">
+          <el-button text size="small" type="primary" icon="Edit">
+            编辑
+          </el-button>
+          <el-button text size="small" type="danger" icon="Delete">
+            删除
+          </el-button>
+        </el-table-column>
+      </el-table>
+      <div class="pagination">分页</div>
     </div>
   </div>
 </template>
@@ -40,6 +81,17 @@ const { usersList } = storeToRefs(systemStore)
 
   .title {
     font-size: 22px;
+  }
+}
+
+.table {
+  :deep(.el-table__cell) {
+    padding: 12px 0;
+  }
+
+  .el-button {
+    margin-left: 0;
+    padding: 5px 8px;
   }
 }
 </style>
